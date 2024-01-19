cdf0372378c26bef3b3a91706abd6bc9b8e8d0d6
Ali Ergüç <ergucali1@gmail.com>
2024-01-20 01:47:19 +0300

    Add Activity Span Status.

--- Observability.ConsoleApp/Services/RequestWebService.cs
+++ Observability.ConsoleApp/Services/RequestWebService.cs
@@ -15,13 +15,23 @@ namespace Observability.ConsoleApp.Services
         internal async Task<int> MakeRequestToGoogle()
         {
             using var activity = ActivitySourceProvider.Source.StartActivity(kind: ActivityKind.Producer, name: "MakeRequestToGoogle");
-            var eventTags = new ActivityTagsCollection();                
-            activity?.AddEvent(new("Request to Google started...",tags: eventTags));
-            var result = await _httpClient.GetAsync("https://www.google.com");
-            var responseContent = await result.Content.ReadAsStringAsync();
-            eventTags.Add("Google request body lenght : ", responseContent.Length);
-            activity?.AddEvent(new("Request to Google finished.!", tags: eventTags));
-            return responseContent.Length;
+
+            try
+            {
+                var eventTags = new ActivityTagsCollection();
+                activity?.AddEvent(new("Request to Google started...", tags: eventTags));
+                var result = await _httpClient.GetAsync("httpss://www.google.com");
+                var responseContent = await result.Content.ReadAsStringAsync();
+                eventTags.Add("Google request body lenght : ", responseContent.Length);
+                activity?.AddEvent(new("Request to Google finished.!", tags: eventTags));
+                return responseContent.Length;
+            }
+            catch (Exception ex)
+            {
+                activity?.SetStatus(ActivityStatusCode.Error, $"Istek yaparken hata oluştu : {ex.Message}");
+                return -1;
+            }
+            
         }
     }
 }
