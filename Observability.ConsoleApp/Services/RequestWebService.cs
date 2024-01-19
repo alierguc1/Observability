750afe8c429d266f3777a041bd0a0d9ee57e818e
Ali Ergüç <ergucali1@gmail.com>
2024-01-20 01:33:28 +0300

    Add Event And Tags.

--- Observability.ConsoleApp/Services/RequestWebService.cs
+++ Observability.ConsoleApp/Services/RequestWebService.cs
@@ -15,8 +15,12 @@ namespace Observability.ConsoleApp.Services
         internal async Task<int> MakeRequestToGoogle()
         {
             using var activity = ActivitySourceProvider.Source.StartActivity(kind: ActivityKind.Producer, name: "MakeRequestToGoogle");
+            var eventTags = new ActivityTagsCollection();                
+            activity?.AddEvent(new("Request to Google started...",tags: eventTags));
             var result = await _httpClient.GetAsync("https://www.google.com");
             var responseContent = await result.Content.ReadAsStringAsync();
+            eventTags.Add("Google request body lenght : ", responseContent.Length);
+            activity?.AddEvent(new("Request to Google finished.!", tags: eventTags));
             return responseContent.Length;
         }
     }
