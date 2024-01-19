f0ee0bc78d6fe19c84b727c7554aec50d8e3c0f6
Ali Ergüç <ergucali1@gmail.com>
2024-01-20 00:29:03 +0300

    downgrade opentelemetry 1.6.0 and implementing trace provider.

---
+++ Observability.ConsoleApp/Constants/OpenTelemetryConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observability.ConsoleApp.Constants
+{
+    internal class OpenTelemetryConstants
+    {
+        internal const string ServiceName = "Observability.ConsoleApp";
+        internal const string ServiceVersion = "1.0.0";
+    }
+}
--- Observability.ConsoleApp/Program.cs
+++ Observability.ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+using Observability.ConsoleApp.Constants;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var traceProvider = Sdk.CreateTracerProviderBuilder()
+                        .ConfigureResource(conf =>
+                        {
+                            conf.AddService(OpenTelemetryConstants.ServiceName,
+                                            OpenTelemetryConstants.ServiceVersion).AddAttributes(new List<KeyValuePair<string, object>>()
+                                            {
+                                                new KeyValuePair<string, object>("host.machineName",Environment.MachineName),
+                                                new KeyValuePair<string, object>("host.os",Environment.OSVersion.VersionString),
+                                                new KeyValuePair<string, object>("host.environment","dev"),
+                                                new KeyValuePair<string, object>("dotnet.version",Environment.Version.ToString())
+                                            });
+                        }).Build();
+
 Console.WriteLine("Hello, World!");
